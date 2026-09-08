80ff3cf347305f9b0c52b89c9ba7d02cadf63841
Dhinesh Jayavel <dhineshjayavel35@gmail.com>
2026-09-08 23:20:06 +0530

    Delete backend directory

--- backend/routes/nearMissRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const nearMissController = require("../controllers/nearMissController");
-const { protect } = require("../middleware/authMiddleware");
-
-router.get("/", protect, nearMissController.listNearMisses);
-router.post("/", protect, nearMissController.createNearMiss);
-
-module.exports = router;
--- backend/routes/receiptRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const receiptController = require("../controllers/receiptController");
-const { protect } = require("../middleware/authMiddleware");
-
-router.get("/:fineId", protect, receiptController.downloadReceipt);
-
-module.exports = router;
